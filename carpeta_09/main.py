ce2d9f10f807c6e268fbc49f60e0a2d539f31ea1
paula <caicedopaso@gmail.com>
2025-04-02 08:38:13 -0500

    cambio

--- carpeta_09/main.py
+++ carpeta_09/main.py
@@ -1,4 +1,2 @@
-# Estudiante: corrige este código y haz un pull request con la versión corregida.
-
-pares = (x for x in range(20) if x % 2 == 0)
+pares = (x for i in range(20) if x % 2 == 0)
 print(pares[3])
